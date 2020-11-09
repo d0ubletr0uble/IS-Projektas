3c486b246f6dd8ceab93ccaa52813060d66cff66
Tomas Odinas <tomas.odinas@ktu.edu>
2020-11-09 16:25:01 +0200

    Added user login

--- routes/web.php
+++ routes/web.php
@@ -59,3 +59,7 @@ Route::get('/git-pull', function () {
     exec('sudo git pull origin code');
     return redirect('/');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
