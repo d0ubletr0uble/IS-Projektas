e97fe950033f4b1e3e8148d1c73bf4dcc7d9777c
nr <nojus.rg@gmail.com>
2020-12-05 16:10:09 +0200

    Admin authentication, user table update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -31,6 +31,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->boolean('is_admin')->nullable();
         });
     }
 
