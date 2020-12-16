3cb7510eeb5ae72c032d7f430fb3ed738a270aaf
Tomas Odinas <tomas.odinas@ktu.edu>
2020-12-16 16:46:23 +0200

    bug fix
    * svetimu zinuciu css pataisymas
    * pataisytas javascript pulso siuntimas zinutem refreshint
    * redaguoti mygtuka rodo tik tam kas sukure grupe
    * pakeisti grupiu ir useriu paveiksleliai

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -60,27 +60,27 @@
                     <ui class="contacts">
                         @foreach($groups as $group)
                             @foreach($membmatymas as $membmatyma)
-                            @if($membmatyma->matymas == 0 && $membmatyma->group_id == $group->id)
-                            <li id="{{$group->id}}" class="group_id" data-my_id="{{$group->getMemberId(Auth::id())}}">
-                                    <div class="d-flex bd-highlight group">
-                                        <div class="img_cont">
-                                            <img
-                                                src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg"
-                                                class="rounded-circle user_img">
-                                            <span class="online_icon offline"></span>
+                                @if($membmatyma->matymas == 0 && $membmatyma->group_id == $group->id)
+                                    <li id="{{$group->id}}" class="group_id"
+                                        data-my_id="{{$group->getMemberId(Auth::id())}}">
+                                        <div class="d-flex bd-highlight group">
+                                            <div class="img_cont">
+                                                <img
+                                                    src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg"
+                                                    class="rounded-circle user_img">
+                                                <span class="online_icon offline"></span>
+                                            </div>
+                                            <div class="user_info">
+                                                <span>{{$group->name}}</span>
+                                                <br>
+                                                @if($group->users_id == Auth::id())
+                                                    <a class="text-warning" href="/messages/groups/{{$group->id}}/edit">Redaguoti</a>
+                                                @endif
+                                            </div>
                                         </div>
-                                        <div class="user_info">
-                                            <span>{{$group->name}}</span>
-                                            <br>
-
-                                                <a class="text-warning"
-                                                   href="/messages/groups/{{$group->id}}/edit">Redaguoti</a>
-
-                                        </div>
-                                    </div>
-                            </li>
-                            @endif
-                        @endforeach
+                                    </li>
+                                @endif
+                            @endforeach
                         @endforeach
                     </ui>
                 </div>
